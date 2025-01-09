d2a366218dcf152956086ba2b997443452368ff5
marlonmelo12 <marlonmoura3000@gmail.com>
2025-01-09 10:46:26 -0300

    inicial

---
+++ src/services/firebaseConnection.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyBUGDCqrE5qKKHXqYj-cw-pKHsdDoVM3tQ",
+  authDomain: "tarefas-c981c.firebaseapp.com",
+  projectId: "tarefas-c981c",
+  storageBucket: "tarefas-c981c.firebasestorage.app",
+  messagingSenderId: "887303831356",
+  appId: "1:887303831356:web:71988135487c4e6c4d6839"
+};
+
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
+export { db };
